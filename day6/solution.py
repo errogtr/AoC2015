8312b9ec8f4144f1bd813915212b6463bd88f5c0
enrico <enrico.dsf@gmail.com>
2024-01-05 19:29:08 +0100

    d6 not optimized

---
+++ day6/solution.py
@@ -0,0 +1,34 @@
+import re
+from itertools import product
+
+
+def parse(line):
+    action, *vertices = re.match(
+        r"(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)", line
+    ).groups()
+    return action, *map(int, vertices)
+
+
+with open("data") as f:
+    instructions = [parse(line) for line in f.read().splitlines()]
+
+size = 1000
+lights = [0] * size**2
+for action, x, y, u, v in instructions:
+    for t, s in product(range(x, u + 1), range(y, v + 1)):
+        match action:
+            case "turn on":
+                lights[t + s * size] = 1
+            case "turn off":
+                lights[t + s * size] = 0
+            case "toggle":
+                lights[t + s * size] = 1 - lights[t + s * size]
+print(sum(lights))
+
+size = 1000
+lights = [0] * size**2
+actions = {"turn on": 1, "turn off": -1, "toggle": 2}
+for action, x, y, u, v in instructions:
+    for t, s in product(range(x, u + 1), range(y, v + 1)):
+        lights[t + s * size] = max(0, lights[t + s * size] + actions[action])
+print(sum(lights))
