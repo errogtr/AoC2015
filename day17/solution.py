a0417c1530bb606200ae7d2fe1de3296caf63ad5
enrico <enrico.dsf@gmail.com>
2024-01-10 00:46:01 +0100

    d17 more optimized solution

--- day17/solution.py
+++ day17/solution.py
@@ -5,9 +5,8 @@ with open("data") as f:
     containers = [int(x) for x in f.read().splitlines()]
 
 # ==== PART 1 ====
-combs = [c for i in range(2, len(containers) + 1) for c in combinations(containers, i) if sum(c) == 150]
-print(len(combs))
+combs = [len([c for c in combinations(containers, i) if sum(c) == 150]) for i in range(2, len(containers) + 1)]
+print(sum(combs))
 
 # ==== PART 2 ====
-min_containers = len(combs[0])
-print(len([c for c in combs if len(c) == min_containers]))
+print(next(c for c in combs if c))
