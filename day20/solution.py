3b9df0d28a717368085b5147101de0e33a102988
enrico <enrico.dsf@gmail.com>
2024-01-12 19:47:08 +0100

    d20 solution

--- day20/solution.py
+++ day20/solution.py
@@ -1,19 +1,24 @@
 from collections import defaultdict
 
-target = 34000000
+
+def visit(house, step, max_range, mult):
+    for j in range(step, max_range, step):
+        house[j] += mult * step
+
+
+with open("data") as f:
+    target = int(f.read())
 
 # ==== PART 1 ====
 house = defaultdict(int)
 for i in range(1, (target // 10) + 1):
-    for j in range(i, (target // 10) + 1, i):
-        house[j] += 10 * i
+    visit(house, i, (target // 10) + 1, 10)
 print(next(k for k, v in house.items() if v >= target))
 
 
 # ==== PART 2 ====
 house = defaultdict(int)
 for i in range(1, (target // 10) + 1):
-    for j in range(i, 51 * i, i):
-        house[j] += 11 * i
+    visit(house, i, 51 * i, 11)
 print(next(k for k, v in house.items() if v >= target))
 
