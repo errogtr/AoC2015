14f28e2f339d57dde3a05b780cc63b73a7932a6c
enrico <enrico.dsf@gmail.com>
2024-01-15 18:45:50 +0100

    d23 solution

---
+++ day23/solution.py
@@ -0,0 +1,32 @@
+def exec(registers):
+    current = 0
+    while True:
+        name, *args = instructions[current].split()
+        match name:
+            case "hlf":
+                registers[args[0] == "b"] /= 2
+            case "tpl":
+                registers[args[0] == "b"] *= 3
+            case "inc":
+                registers[args[0] == "b"] += 1
+            case "jmp":
+                current += int(args[0]) - 1
+            case "jie":
+                if registers[args[0].strip(",") == "b"] % 2 == 0:
+                    current += int(args[1]) - 1
+            case "jio":
+                if registers[args[0].strip(",") == "b"] == 1:
+                    current += int(args[1]) - 1
+        current += 1
+        if current == len(instructions):
+            return registers[1]
+
+
+with open("data") as f:
+    instructions = f.read().splitlines()
+
+# ==== PART 1 ====
+print(exec([0, 0]))
+
+# ==== PART 2 ====
+print(exec([1, 0]))
