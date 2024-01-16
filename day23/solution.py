04a7d2f6329bb435ce4e71ab5e45c633dea936f9
enrico <enrico.dsf@gmail.com>
2024-01-16 17:02:37 +0100

    d23 refactor

--- day23/solution.py
+++ day23/solution.py
@@ -4,29 +4,29 @@ def exec(registers):
         name, *args = instructions[current].replace(",", "").split()
         match name:
             case "hlf":
-                registers[args[0] == "b"] /= 2
+                registers[args[0]] /= 2
             case "tpl":
-                registers[args[0] == "b"] *= 3
+                registers[args[0]] *= 3
             case "inc":
-                registers[args[0] == "b"] += 1
+                registers[args[0]] += 1
             case "jmp":
                 current += int(args[0]) - 1
             case "jie":
-                if registers[args[0] == "b"] % 2 == 0:
+                if registers[args[0]] % 2 == 0:
                     current += int(args[1]) - 1
             case "jio":
-                if registers[args[0] == "b"] == 1:
+                if registers[args[0]] == 1:
                     current += int(args[1]) - 1
         current += 1
         if current == len(instructions):
-            return registers[1]
+            return registers["b"]
 
 
 with open("data") as f:
     instructions = f.read().splitlines()
 
 # ==== PART 1 ====
-print(exec([0, 0]))
+print(exec({"a": 0, "b": 0}))
 
 # ==== PART 2 ====
-print(exec([1, 0]))
+print(exec({"a": 1, "b": 0}))
