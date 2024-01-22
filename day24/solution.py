033e8a65358f0f7cdbe3785eaba6e2c812d7406e
enrico <enrico.dsf@gmail.com>
2024-01-22 23:24:07 +0100

    d24 syntactic sugar

--- day24/solution.py
+++ day24/solution.py
@@ -2,11 +2,10 @@ from itertools import combinations
 from math import prod
 
 
-def entanglements(nums, target):
+def entanglements(nums, s):
     for k in range(1, len(nums)):
-        entanglements = [prod(c) for c in combinations(nums, k) if sum(c) == target]
-        if entanglements:
-            return min(entanglements)
+        if qe := min([prod(c) for c in combinations(nums, k) if sum(c) == s], default=0):
+            return qe
 
 
 with open("data") as f:
